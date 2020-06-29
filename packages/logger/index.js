8d3f57a07d97445e87990726b4897ffed21fa943
Sergey Sharykhin <sergey.sharykhin@ix.co>
2020-06-29 17:37:33 +0300

    added opentracing

--- packages/logger/index.js
+++ packages/logger/index.js
@@ -1,3 +1,5 @@
+const { FORMAT_HTTP_HEADERS } = require('opentracing');
+
 exports.Log = function (message) {
     console.log(`your message is ${message}`);
 }
--- packages/tracer/index.js
+++ packages/tracer/index.js
@@ -1,3 +1,5 @@
+const { FORMAT_HTTP_HEADERS } = require('opentracing');
+
 exports.trace = function (id) {
-    console.log(`Traced ${id} token`);
+    console.log(`Traced ${id} token: ${FORMAT_HTTP_HEADERS}`);
 }
