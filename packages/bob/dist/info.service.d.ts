2ba939bfb90d7082b72741476d5519c64d32a8fa
Sergey Sharykhin <sergey.sharykhin@ix.co>
2020-06-30 15:03:02 +0300

    added bob package

---
+++ packages/bob/dist/info.service.d.ts
@@ -0,0 +1,5 @@
+export declare class InfoService {
+    readonly name: string;
+    constructor(name: string);
+    getName(): string;
+}
---
+++ packages/bob/dist/info.service.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.InfoService = void 0;
+class InfoService {
+    constructor(name) {
+        this.name = name;
+    }
+    getName() {
+        return this.name;
+    }
+}
+exports.InfoService = InfoService;
+//# sourceMappingURL=info.service.js.map
